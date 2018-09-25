56db1109fcfdc2f65c4ac9be001d070933e26d54
Rahman Muhammad Saleh <rahman9600@gmail.com>
2018-09-25 09:29:05 +0700

    - add navigation bottom layout - add appbarlayout - add color in color.xml - add style in style.xml - add menu directory - add image icon

--- app/src/main/java/com/azhamudev/indonesianfood/MainActivity.kt
+++ app/src/main/java/com/azhamudev/indonesianfood/MainActivity.kt
@@ -14,6 +14,7 @@ import org.jetbrains.anko.startActivity
 class MainActivity : AppCompatActivity() {
 
     private var items: MutableList<Item> = mutableListOf()
+    //private var BottomNavigationView:
 
     private fun initData() {
         val name = resources.getStringArray(food_name)
@@ -46,4 +47,5 @@ class MainActivity : AppCompatActivity() {
 
         }
     }
+
 }
